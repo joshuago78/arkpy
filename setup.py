6acb77c426e9e33d5c1e560f2801568013dcfdaa
Joshua Gomez <gomezjn@usc.edu>
2014-07-23 15:27:53 -0700

    increased version number from 0.5 to 0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='arkpy',
-    version='0.5',
+    version='0.6',
     description="""Simple tools built around the Archival Resource Key
     and Noid specifications, allowing minting and validating of ARKs
     via code and the command-line.""",
